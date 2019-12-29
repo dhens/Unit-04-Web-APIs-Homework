70e0009f2b02600b14dbb7dd9ae1f3eba6e8659d
dhens <dawsonhensel@gmail.com>
2019-12-29 09:16:58 -0800

    made navbar that complies with hw standards and started grid system for the index page using bootstrap

---
+++ assets/js/quiz.js
@@ -0,0 +1,4 @@
+let startPageText = document.getElementsByClassName("start-page-text");
+console.log(startPageText);
+let startButton = document.getElementsByClassName("start-quiz-button");
+console.log(startButton);
