5c76661b5f1d385b17e934549b3298cbedff59f6
dhens <dawsonhensel@gmail.com>
2019-12-29 11:08:17 -0800

    removed extra unneeded css, made it so when you click the start quiz button it removes the splash screen info out of the DOM, and changed start-page-text to an id rather than a class to get it to work with my js remove child function for the button click event

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,4 +1,11 @@
-let startPageText = document.getElementsByClassName("start-page-text");
-console.log(startPageText);
-let startButton = document.getElementsByClassName("start-quiz-button");
+let startPageText = document.getElementById("start-page-text");
+let startButton = document.getElementById("start-quiz-button");
 console.log(startButton);
+
+
+startButton.addEventListener("click", function() {
+    while (startPageText.firstChild) {
+        startPageText.firstChild.remove();
+    }
+    console.log("Button pressed")
+    });
