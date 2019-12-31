7fe658a21dcf30369e63fc8b1da8de12031a27ae
dhens <dawsonhensel@gmail.com>
2019-12-30 16:13:35 -0800

    made renderTitle and renderAnswerButtons functions for making future code adaptations easier

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -15,40 +15,33 @@ startButton.addEventListener("click", function() {
 
     // As long as there are items in questions array, populate the page with the title and choices
     for (let i = 0; i < questions.length; i++) {
-        debugger;
-        let qTitle = questions[i].title;
 
-        // Create Title Text
-        let qTitleElement = document.createElement("h2");                       // Create a <h2> node
-        let qTitleText = document.createTextNode(qTitle);                       // Create a text node
-        qTitleElement.appendChild(qTitleText);                                  // Append the text to <li>
-        document.getElementById("start-page-text").appendChild(qTitleElement);  // Append <h2> to <ul> with id="myList" 
+        renderTitle(i);
 
         for (let ii = 0; ii < questions[i].choices.length; ii++) {
 
-            let btnContent = questions[i].choices[ii];
-            console.log("Current index in array: " + btnContent)
-
-            // Create Buttons
-            let btnElement = document.createElement("button");                       // Create a <li> node
-            btnElement.setAttribute("id", "answer-button")
-            let btnText = document.createTextNode(btnContent);                       // Create a text node
-            btnElement.appendChild(btnText);                                         // Append the text to <li>
-            startPageText.appendChild(btnElement);          // Append <li> to <ul> with id="myList" 
+            renderAnswerButtons(i, ii);
 
         }
-
     }
 
     });
 
-    // function renderQuestion(index) {
-    //             var qTitle = questions[i].title;
+    function renderTitle(index) {
+        let qTitle = questions[index].title;
+        let qTitleElement = document.createElement("h2");                     
+        let qTitleText = document.createTextNode(qTitle);                      
+        qTitleElement.appendChild(qTitleText);                                  
+        document.getElementById("start-page-text").appendChild(qTitleElement);  
 
-    //     let qTitleElement = document.createElement("h2");                       // Create a <li> node
-    //     let qTitleText = document.createTextNode(qTitle);                       // Create a text node
-    //     qTitleElement.appendChild(qTitleText);                                  // Append the text to <li>
-    //     document.getElementById("start-page-text").appendChild(qTitleElement);  // Append <li> to <ul> with id="myList" 
+    }
 
-
-    // }
+    function renderAnswerButtons(index, index2) {
+        let btnContent = questions[index].choices[index2];
+        let btnElement = document.createElement("button");                     
+        btnElement.setAttribute("id", "answer-button")
+        let btnText = document.createTextNode(btnContent);                      
+        btnElement.appendChild(btnText);                                         
+        startPageText.appendChild(btnElement);
+        console.log("Current index in array: " + btnContent)
+    }          
