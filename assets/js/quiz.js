35621851db9fcd177bc7face988a4c8948cf3ae6
dhens <dawsonhensel@gmail.com>
2019-12-31 15:32:50 -0800

    the rendering of the current question title, as well as the button choice options, is no longer run through a for loop, but the title and buttons each have their own function for rendering to the page. buttons no longer append to start-page-text id, but a new id called answer-buttons. this is used for event delegation and to not trigger unwanted code runs when you click the init 'Start Quiz' button. made function parameter names easier to understand

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,47 +1,69 @@
-console.log("Successfully loaded quiz.js")
+console.log("Successfully loaded quiz.js");
 
 // Declare locations
-let startPageText = document.getElementById("start-page-text");
-let startButton = document.getElementById("start-quiz-button");
+const startPageText = document.getElementById("start-page-text");
+const startButton = document.getElementById("start-quiz-button");
 
 startButton.addEventListener("click", function() {
-    console.log("Start Quiz button pressed")
+    console.log("Start Quiz button clicked");
 
     // For each child of start-page-text id, remove it from the DOM
     while (startPageText.firstChild) {
         startPageText.firstChild.remove();
     }
 
+    // Define iteration counters (i, ii) for renderTitle and renderAnswerButtons's parameter arguments
+    // We later update the values of both from within each function using titleIndex++ and btnChoiceIndex++ respectively 
+    let i = 0;
+    let ii = 0;
 
-    // As long as there are items in questions array, populate the page with the title and choices
-    for (let i = 0; i < questions.length; i++) {
+    renderTitle(i);
+    renderAnswerButtons(i, ii);
+    const answerButtons = document.getElementById("answer-buttons");
 
-        renderTitle(i);
 
-        for (let ii = 0; ii < questions[i].choices.length; ii++) {
+answerButtons.addEventListener("click", function() {
+    event.preventDefault();
+    if(event.target.matches("button")) {
+        console.log("One of the answers buttons was clicked");
 
-            renderAnswerButtons(i, ii);
-
-        }
     }
 
-    });
+});
+});
 
-    function renderTitle(index) {
-        let qTitle = questions[index].title;
-        let qTitleElement = document.createElement("h2");                     
+    function renderTitle(titleIndex) {
+        let qTitle = questions[titleIndex].title;
+        let qTitleElement = document.createElement("h2"); 
         let qTitleText = document.createTextNode(qTitle);                      
         qTitleElement.appendChild(qTitleText);                                  
-        document.getElementById("start-page-text").appendChild(qTitleElement);  
-
+        startPageText.appendChild(qTitleElement);
+        console.log("Rendered title: " + qTitle);
+        titleIndex++;
     }
 
-    function renderAnswerButtons(index, index2) {
-        let btnContent = questions[index].choices[index2];
-        let btnElement = document.createElement("button");                     
-        btnElement.setAttribute("id", "answer-button")
-        let btnText = document.createTextNode(btnContent);                      
-        btnElement.appendChild(btnText);                                         
-        startPageText.appendChild(btnElement);
-        console.log("Current index in array: " + btnContent)
+    function renderAnswerButtons(titleIndex, btnChoiceIndex) {
+
+        let answerBtnDiv = document.createElement("DIV"); 
+        answerBtnDiv.setAttribute("id", "answer-buttons");
+        startPageText.appendChild(answerBtnDiv);
+        const answerButtons = document.getElementById("answer-buttons");
+
+        // Loop through each index in the current object's "choices" array and create a button with text from the index
+        for (let choiceIterationCount = 0; choiceIterationCount < questions[titleIndex].choices.length; choiceIterationCount++) {
+
+            // Define variable that is selecting the index of the val of titleIndex, which starts at 0
+            // Ex: questions[0].choices[0] = strings
+            let btnContent = questions[titleIndex].choices[btnChoiceIndex];
+            let btnElement = document.createElement("button");
+            let btnText = document.createTextNode(btnContent);                      
+            
+            // Create an ID attribute for our button. Used for styling.            
+            btnElement.setAttribute("id", "answer-button");
+
+            btnElement.appendChild(btnText);
+            answerButtons.appendChild(btnElement);
+            console.log("Current index in array: " + btnContent);
+            btnChoiceIndex++;
+        }
     }          
